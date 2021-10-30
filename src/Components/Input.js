c17358821cbfd36b6a79664e50f31a8eaa8367d3
MohamedJH <mohamedjahha0@gmail.com>
2021-10-30 11:21:15 +0100

    upload

---
+++ src/Components/Input.js
@@ -0,0 +1,94 @@
+
+
+export const moviesList=
+[
+  {
+    id:1,
+    title:"OSEAM".toLowerCase(), 
+    description:"Livrés à eux-mêmes depuis la disparition de leur mère, Gamie, une jeune fille aveugle, et son petit frère Gil-Sun sont recueillis par un moine bouddhiste. Si la fillette trouve au temple une place en accord avec sa discrétion, Gil-Sun ne tarde pas à perturber, à la force de son enthousiasme enfantin, la tranquillité des lieux.Le petit garçon ne comprend pas pourquoi sa mère rend visite à sa soeur dans ses rêves, mais ne vient jamais le voir, lui qui a pourtant les yeux grands ouverts. Pour la retrouver, il décide de suivre le moine dans un long voyage initiatique.", 
+    posterUrl:"https://img0.cine974.com/images/affiches/grand/oseam-m6UcJ6nf.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/KtL5CQngHPM",
+    
+    note:3
+  },
+  {
+    id:2,
+    title:"THE GARDEN OF WORDS".toLowerCase(), 
+    description:"Takao, qui est en apprentissage pour devenir cordonnier, sèche les cours et dessine des chaussures dans un jardin de style japonais. Il y rencontre une mystérieuse femme, Yukino, qui est plus âgée que lui. Par la suite, et sans se donner rendez-vous, ils commencent à se voir encore et encore mais seulement les jours de pluie. Ils finissent par discuter ensemble et s'ouvrent l'un à l'autre. Mais la fin de la saison des pluies approche...", 
+    posterUrl:"https://img0.cine974.com/images/affiches/grand/the-garden-of-words-ADDU4Lf6.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/jR5szRoUUPc",
+    note:4
+  },
+  {
+    id:3,
+    title:"LE GARÇON ET LA BÊTE".toLowerCase(), 
+    description:"Shibuya, le monde des humains, et Jutengai, le monde des Bêtes... C'est l'histoire d'un garçon solitaire et d'une Bête seule, qui vivent chacun dans deux mondes séparés. Un jour, le garçon se perd dans le monde des Bêtes où il devient le disciple de la Bête Kumatetsu qui lui donne le nom de Kyuta. ", 
+    posterUrl:"https://img0.cine974.com/images/affiches/grand/le-gar--on-et-la-b--te-r1CYjwDX.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/TNXc9_cAkyg",
+    
+    note:5
+  },{
+    id:4,
+    title:"The Old Guard".toLowerCase(), 
+    description:"Une petite bande soudée de mercenaires immortels, dirigée par la redoutable Andy, se bat depuis des siècles pour protéger les humains. Mais tandis que le groupe est engagé pour une mission des plus périlleuses, ses pouvoirs hors du commun sont soudain révélés au grand jour. C'est alors qu'Andy et Nile, tout dernier soldat à avoir rejoint l'équipe, doivent tout mettre en œuvre pour neutraliser leurs ennemis. Car ces derniers ne reculeront devant rien pour détourner les pouvoirs des immortels à leur profit.", 
+    posterUrl:"https://fr.web.img4.acsta.net/pictures/20/05/26/09/44/5988886.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/aK-X2d0lJ_s?start=9",
+    note:3.6
+  },
+  {
+    id:5,
+    title:"THE GARDEN OF WORDS".toLowerCase(), 
+    description:"Takao, qui est en apprentissage pour devenir cordonnier, sèche les cours et dessine des chaussures dans un jardin de style japonais. Il y rencontre une mystérieuse femme, Yukino, qui est plus âgée que lui. Par la suite, et sans se donner rendez-vous, ils commencent à se voir encore et encore mais seulement les jours de pluie. Ils finissent par discuter ensemble et s'ouvrent l'un à l'autre. Mais la fin de la saison des pluies approche...", 
+    posterUrl:"https://img0.cine974.com/images/affiches/grand/the-garden-of-words-ADDU4Lf6.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/jR5szRoUUPc",
+    note:4.2
+  },
+  {
+    id:6,
+    title:"TENET".toLowerCase(), 
+    description:"Muni d'un seul mot – Tenet – et décidé à se battre pour sauver le monde, notre protagoniste sillonne l'univers crépusculaire de l'espionnage international. Sa mission le projettera dans une dimension qui dépasse le temps. Pourtant, il ne s'agit pas d'un voyage dans le temps, mais d'un renversement temporel…", 
+    posterUrl:"https://fr.web.img2.acsta.net/pictures/20/08/03/12/15/2118693.jpg", 
+    trailerUrl:"https://www.youtube.com/embed/AZGcmvrTX9M",
+    note:2.8
+  },
+  {
+    id:7,
+    title:"Bloodshot".toLowerCase(), 
+    description:"Ray Garrison est un soldat tué en mission, et ramené à la vie par RST Corporations, l(entreprise qui l’a transformé en super-humain. Des nanotechnologies coulent désormais dans ses veines, ce qui le rend invincible. Il est plus fort que jamais et capable de guérir instantanément de ses blessures. Mais RST Corporation ne contrôle pas que son corps… Ils ont également la main sur son esprit et ses souvenirs. Ray ne peut distinguer ce qui est réel de ce qui ne l’est pas – mais sa mission est désormais de le découvrir.", 
+    posterUrl:"https://www.cinehorizons.net/sites/default/files/affiches/49968461-bloodshot.jpg",  
+    trailerUrl:"https://www.youtube.com/embed/vOUVVDWdXbo",
+    note:1.7
+  },
+  {
+    id:8,
+    title:"Scooby !".toLowerCase(), 
+    description:"Dans SCOOBY!, on découvre comment Scooby et Sammy, amis pour la vie, se sont rencontrés et associés aux détectives en herbe Fred, Velma et Daphné pour créer la célèbre équipe Mystère et Cie. Après avoir résolu des centaines d'affaires et vécu d'innombrables aventures, Scooby et sa bande doivent désormais s'attaquer à leur énigme la plus redoutable : un complot destiné à déchaîner les forces du chien-fantôme Cerberus.", 
+    posterUrl:"https://img0.cine974.com/images/affiches/grand/le-gar--on-et-la-b--te-r1CYjwDX.jpg",  
+    trailerUrl:"https://www.youtube.com/embed/vf1aW1z437I",
+    note:2
+  },
+  {
+    id:9,
+    title:"Undercover".toLowerCase(), 
+    description:"À Detroit, dans les années 80, au plus fort de la guerre contre l’épidémie de crack, voici l’histoire vraie d’un père d’origine modeste, Richard Wershe, et de son fils, Rick Jr., un adolescent qui devint informateur pour le compte de la police, avant de s’impliquer du côté des dealers pour se retrouver abandonné par ceux qui l’avaient utilisé et condamné à finir ses jours en prison.", 
+    posterUrl:"https://www.cinehorizons.net/sites/default/files/styles/affiches-m/public/affiches/1560713319-undercover-une-histoire-vraie.jpg?itok=TNnIJu9r",  
+    trailerUrl:"https://www.youtube.com/embed/USP7sUNfLIs",
+    note:3
+  },
+  {
+    id:10,
+    title:"Creed II".toLowerCase(), 
+    description:"La vie est devenue un numéro d'équilibriste pour Adonis Creed. Entre ses obligations personnelles et son entraînement pour son prochain grand match, il est à la croisée des chemins. Et l'enjeu du combat est d'autant plus élevé que son rival est lié au passé de sa famille. Mais il peut compter sur la présence de Rocky Balboa à ses côtés : avec lui, il comprendra ce qui vaut la peine de se battre et découvrira qu'il n'y a rien de plus important que les valeurs familiales.", 
+    posterUrl:"https://www.cinehorizons.net/sites/default/files/styles/affiches-m/public/affiches/1988395237-creed-ii.jpeg?itok=5D8F7U96",  
+    trailerUrl:"https://www.youtube.com/embed/cPNVNqn4T9I?start=1",
+    note:4.6
+  },
+  {
+    id:11,
+    title:"Colette".toLowerCase(), 
+    description:"1893. Malgré leurs quatorze ans d'écart, Gabrielle Sidonie Colette, jeune fille à l'esprit rebelle, épouse Willy, écrivain aussi égocentrique que séducteur. Grâce à ses relations, elle découvre le milieu artistique parisien qui stimule sa propre créativité. Sachant repérer les talents mieux que quiconque, Willy autorise Colette à écrire – à condition qu'il signe ses romans à sa place. Suite au triomphe de la série des Claudine, il ne tarde d'ailleurs pas à devenir célèbre. Pourtant, tandis que les infidélités de Willy pèsent sur le couple, Colette souffre de plus en plus de ne pas être reconnue pour son œuvre…", 
+    posterUrl:"https://www.cinehorizons.net/sites/default/files/styles/affiches-m/public/affiches/1011101910-colette.jpg?itok=TxxUYvUA",  
+    trailerUrl:"https://www.youtube.com/embed/Mqdyyk-iOvY?start=17",
+    note:4.9
+  },
+]
